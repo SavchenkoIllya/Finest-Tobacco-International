a973aedaf30f1acc52d16eecbd6fa36edb4e5fba
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-06-29 00:54:36 +0200

    pipeline test 3 and layout fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,44 @@
-import { redirect } from "next/navigation";
+import {
+  About,
+  AgeModal,
+  ContactsSection,
+  Header,
+  Hero,
+  Production,
+  ScrollIndicator,
+} from "@/app/ui";
+import { Global } from "@/app/types";
+import { axiosClient, StrapiRoutes } from "@/app/lib";
+import { globalQuery } from "@/app/actions";
 
-export default function Home() {
-  redirect("/en");
+export async function generateStaticParams() {
+  return [{ lang: "en" }, { lang: "ru" }];
+}
+
+export default async function Home() {
+  let data: Global | null = null;
+
+  try {
+    const res = await axiosClient(
+      `${StrapiRoutes.GET_GLOBALS}?${globalQuery}`,
+      { method: "GET" },
+    );
+    data = await res.data.data;
+  } catch (e) {
+    console.error(e);
+  }
+
+  return (
+    <>
+      <AgeModal />
+      <ScrollIndicator />
+      <div className={"relative "}>
+        <Header header_content={data?.Header} />
+        <Hero video_url={data?.video?.url} />
+        <About />
+        <Production />
+        <ContactsSection />
+      </div>
+    </>
+  );
 }
