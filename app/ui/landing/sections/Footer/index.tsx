9503ae98893714331e4f10a137fbe297dec3f6ea
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-02 15:10:31 +0200

    Minor visual fix

--- app/ui/landing/sections/Footer/index.tsx
+++ app/ui/landing/sections/Footer/index.tsx
@@ -10,7 +10,7 @@ export const FooterSection = ({
   footer_content: SharedFooter;
 }) => {
   return (
-    <section className={"bg-primary p-12"}>
+    <section className={"bg-primary p-12 mt-12"}>
       <div className="flex flex-col lg:flex-row">
         <div className="w-full lg:w-1/3 mb-4 lg:mb-0">
           <p className="text-secondary max-w-xs">
