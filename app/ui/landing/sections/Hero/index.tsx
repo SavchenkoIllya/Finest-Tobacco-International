c9159f2a202f5de7ad085e5ddd3eae2ca4e9ad80
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-03 18:36:07 +0200

    Cloud stream video fix

--- app/ui/landing/sections/Hero/index.tsx
+++ app/ui/landing/sections/Hero/index.tsx
@@ -1,25 +1,26 @@
 "use client";
 import { LandingSections } from "@/app/lib";
 import { ScrollButton } from "@/app/ui/landing/sections/Hero/components/ScrollButton";
-import { Stream } from "@cloudflare/stream-react";
 
 export const Hero = ({ video_url }: { video_url?: string }) => {
-  const videoIdOrSignedToken = "563571594c01d35726747f61578d03f3";
-
   return (
     <section
       id={LandingSections.HERO}
       className="relative min-h-screen overflow-hidden"
     >
-      <Stream
-        className={"object-cover w-full h-full"}
-        muted
-        autoplay
-        loop
-        src={videoIdOrSignedToken}
-      />
-      <div className="z-20 absolute bottom-0 mb-10 left-[50%] transform -translate-x-1/2 flex flex-col justify-center gap-2">
-        <ScrollButton text={"↓"} />
+      <div className="absolute inset-0 z-0">
+        <video
+          className="w-full h-full min-w-full min-h-full z-[-1]"
+          src="https://customer-835voqu02pldhkl9.cloudflarestream.com/563571594c01d35726747f61578d03f3/downloads/default.mp4"
+          muted
+          autoPlay
+          loop
+          playsInline
+        />
+      </div>
+
+      <div className="z-20 absolute bottom-0 mb-10 left-1/2 transform -translate-x-1/2 flex flex-col justify-center gap-2">
+        <ScrollButton text="↓" />
       </div>
     </section>
   );
