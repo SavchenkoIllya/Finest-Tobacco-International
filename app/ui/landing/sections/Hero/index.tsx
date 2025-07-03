34c19d810e40659e865696319167d433e630a049
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-03 17:50:28 +0200

    Cloudflare video test 1

--- app/ui/landing/sections/Hero/index.tsx
+++ app/ui/landing/sections/Hero/index.tsx
@@ -1,18 +1,23 @@
 "use client";
 import { LandingSections } from "@/app/lib";
 import { ScrollButton } from "@/app/ui/landing/sections/Hero/components/ScrollButton";
+import { Stream } from "@cloudflare/stream-react";
 
 export const Hero = ({ video_url }: { video_url?: string }) => {
-  if (!video_url) return null;
+  const videoIdOrSignedToken = "563571594c01d35726747f61578d03f3";
 
   return (
     <section
       id={LandingSections.HERO}
       className="relative min-h-screen overflow-hidden"
     >
-      <video autoplay muted loop playsinline className={"object-cover, w-full h-full"}>
-        <source src="https://customer-835voqu02pldhk19.cloudflarestream.com/56357159dc01d35726747f61578d03f3/manifest/video.m3u8" type="application/x-mpegURL">
-      </video>
+      <Stream
+        className={"object-cover w-full h-full"}
+        muted
+        autoplay
+        loop
+        src={videoIdOrSignedToken}
+      />
       <div className="z-20 absolute bottom-0 mb-10 left-[50%] transform -translate-x-1/2 flex flex-col justify-center gap-2">
         <ScrollButton text={"↓"} />
       </div>
